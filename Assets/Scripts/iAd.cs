90a634f6bb8353a239bc10947ea191580565bec3
Adamo Morone <adamo.morone@gmail.com>
2015-01-20 22:26:39 -0200

    iAd Implemented

---
+++ Assets/Scripts/iAd.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class iAd : MonoBehaviour {
+
+	private ADBannerView banner = null;
+	void Start()
+	{
+		banner = new ADBannerView(ADBannerView.Type.Banner, ADBannerView.Layout.Bottom);
+		ADBannerView.onBannerWasClicked += OnBannerClicked;
+		ADBannerView.onBannerWasLoaded  += OnBannerLoaded;
+	}
+	void OnBannerClicked()
+	{
+		Debug.Log("Clicked!\n");
+	}
+	void OnBannerLoaded()
+	{
+		Debug.Log("Loaded!\n");
+		banner.visible = true;
+	}
+}
